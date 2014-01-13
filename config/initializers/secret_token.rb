f377def266d390499994e304fe8ef39efc761086
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2014-01-13 09:22:12 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fts1::Application.config.secret_key_base = '802cad7ed1de53ab4df29894c22985c9ee3e5b793dd858ec0ee90061c0d1c2d1bb5b5c979a357d326fea95e505d2742e585031fe476b9c1e55ccdca307ecd1bf'
