d48e3a990362a3aab57547be22bfa50bb3558a90
Viet Nguyen <nguyen.tiep.viet@framgia.com>
2014-01-13 14:22:16 +0700

    Task#client_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,27 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
 
   def show
-
+    @user = User.find params[:id]
   end
 
   def edit
   end
 
   def update
-    
+    if @user.update_attributes! user_params
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
 
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
     # Before filters
 
     def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  attr_accessor :not_validates_password
+  validates :password, length: { minimum: 6 }, unless: :not_validates_password
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
