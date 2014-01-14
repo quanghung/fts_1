ac16bbb9c7e601ea529a550fa1afd2db229a8d24
TrAnh <nguyen.van.tran.anh@framgia.com>
2014-01-14 18:11:19 +0700

    refs task#question2

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -6,11 +6,29 @@ class Admin::QuestionsController < ApplicationController
   end
 
   def new
-  	
+    @subjects = Subject.all
+    if params[:question_type].present?
+      @question = Question.new question_text: params[:question_text], subject_id: params[:subject_id], question_type: params[:question_type]
+      if params[:question_type] == "true"
+        @question.answer_text = AnswerText.new 
+      else
+        4.times {@question.answer_choices.build(status: false)}
+      end
+      render partial: "admin/questions/question_form" 
+    else
+      @question = Question.new
+    end
   end
 
   def create
-  	
+  	question = Question.new question_choices_params
+    if question.save
+      flash[:success] = I18n.t ".messages.add_project.success"
+      redirect_to admin_questions_path
+    else
+      flash[:success] = I18n.t ".messages.add_project.unsuccess"
+      render "new"
+    end
   end
 
   def update
@@ -24,4 +42,15 @@ class Admin::QuestionsController < ApplicationController
   def destroy
   	
   end
+
+  private
+    def question_choices_params
+      params.require(:question).permit :question_text, :subject_id,
+      question_answer_choices_attributes: [:text, :status]
+    end
+
+    def question_text_params
+      params.require(:question).permit :question_text, :subject_id,
+      question_answer_text_attributes: [:text]
+    end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ActiveRecord::Base
   has_many :answer_choices
+  accepts_nested_attributes_for :answer_choices
   has_one :answer_text
+  accepts_nested_attributes_for :answer_text
   has_many :exam_questions
   has_many :exams, through: :exam_questions
   belongs_to :subject
